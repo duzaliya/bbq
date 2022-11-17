f2b9f5bbbb968f0cc9f7a3d67c800c947c388f68
ZaliyaD <zaliya.dukina@gmail.com>
2022-11-17 19:56:11 +0300

    First alpha version ready

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  has_many :events
+
+  before_validation :set_name, on: :create
+  validates :name, presence: true, length: {maximum: 35}
+
+  private
+
+  def set_name
+    self.name = "Кое-кто №#{rand(777)}" if self.name.blank?
+  end
+end
