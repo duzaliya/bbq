c75b9a7429980ce0e3fd082590df0eb1e5aed3a9
ZaliyaD <zaliya.dukina@gmail.com>
2023-01-30 20:01:46 +0300

    Pincode for events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_08_102024) do
+ActiveRecord::Schema.define(version: 2023_01_28_181727) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2022_12_08_102024) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id", null: false
+    t.string "pincode"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
