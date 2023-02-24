df89a832c6157c64a534276e8cab1feaae19cb95
ZaliyaD <zaliya.dukina@gmail.com>
2023-02-24 18:34:07 +0300

    Return js setting

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ import 'bootstrap/dist/js/bootstrap'
 import 'bootstrap/dist/css/bootstrap'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import 'stylesheets/application'
-import 'lightbox2/dist/js/lightbox-plus-jquery'
+import 'lightbox.js'
 
 Rails.start()
 Turbolinks.start()
