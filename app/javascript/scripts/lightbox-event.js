5597b6d2bb7776b5938440ebd9fb17c878d2241b
ZaliyaD <zaliya.dukina@gmail.com>
2023-03-17 14:58:20 +0300

    Adding turbolinks-visit-control

--- app/javascript/scripts/lightbox-event.js
+++
@@ -1,9 +0,0 @@
-import Lightbox from 'bs5-lightbox'
-
-document.addEventListener('turbolinks:load', () => {
-  document.querySelectorAll('.lightbox').forEach((el) => el.addEventListener('click', (e) => {
-    e.preventDefault()
-    const lightbox = new Lightbox(el)
-    lightbox.show()
-  }))
-})
